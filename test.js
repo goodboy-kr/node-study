958a471cfb0ae6d2c8de5a1c36fba39e3e89f33a
good-boy <llaayy54@gmail.com>
2020-08-01 17:48:27 +0900

    0801

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
-var users = [{ name: 'a', age: 10 }, { name: 'b', age: 20 }, { name: 'c', age: 30 }, { name: 'd', age: 40 }];
+function add(a, b, callback) {
+    var result = a + b;
+    callback(result);
 
-console.log("length : %d",users.length);
-console.dir(users);
+    var history = function () {
+        return a + ' + ' + b + ' = ' + result;
+    };
+    return history;
+}
 
-var users2= users.slice(1,3);
-console.log("2")
-console.dir(users2);
 
+ var x = add(11,22,function(R){
+    console.log('parameter callbacked');
+    console.log('add(11,22) result : %d', R);
+});
 
-var users3 = users2.slice(1);
-console.log("3")
-console.dir(users3);
+console.log('final answer : ' + x())
