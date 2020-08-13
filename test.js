4881b56ad11e7941a744bbc0d0d3d2a7f40e0c8d
good-boy <llaayy54@gmail.com>
2020-08-13 10:33:51 +0900

    new update

--- test.js
+++ test.js
@@ -1,17 +1,26 @@
 function add(a, b, callback) {
     var result = a + b;
-    callback(result);
+    callback(result)
 
+    var count = 0;
     var history = function () {
-        return a + ' + ' + b + ' = ' + result;
+        count++;
+        console.log("count in add : " + count)
+        return count + ':' + a + '+' + b + '=' + result;
     };
-    return history;
+    return history
 }
 
-
- var x = add(11,22,function(R){
-    console.log('parameter callbacked');
-    console.log('add(11,22) result : %d', R);
+var add_history = add(10, 10, function (result) {
+    console.log('parameter callback function called');
+    console.log('add (10, 10) result : %d', result);
 });
 
-console.log('final answer : ' + x())
+console.log('answer : ' + add_history());
+console.log('answer : ' + add_history());
+console.log('answer : ' + add_history());
+
+console.log("end")
+add(10, 10, function (x) {console.log(x)})()
+add(10, 10, function (x) {console.log(x)})()
+add(10, 10, function (x) {console.log(x)})()
--- test1.js
+++ test1.js
@@ -1,8 +1,10 @@
-var calc = require('./calc');
-var calc2 = require('./calc2');
+function plus(a) {
+    return function (x) {
+        return a + x;
+    }
+}
+var plus3 = plus(3);
+var plus5 = plus(5);
 
-
-console.log('after module : %d', calc.add(10, 10));
-
-console.log('calc2 add : %d', calc2.add(10,30));
-console.log('calc2 multiply : %d', calc2.multiply(10,30));
+console.log(plus3)
+console.log(plus5)
